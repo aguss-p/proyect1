ce5cad4f61534bbd4fb3737f98495ce59b93ab3d
Maukr <maurifazzano@gmail.com>
2021-04-25 18:40:49 -0300

    cambios formulario quedando facha

--- src/components/IniciarSesion.js
+++ src/components/IniciarSesion.js
@@ -1,13 +1,19 @@
 import React, {useState, useEffect} from 'react';
+import Button from '@material-ui/core/Button';
 
 const IniciarSesion = () => {
 
     return (
-        <form id="iniciar-form">
-            <input type="email" placeholder="Ingresa email" name="email" id="email"/>
-            <input type="password" placeholder="Ingresa contraseña" name="password" id="password"/>
-            <button>INICIAR SESION</button>
-        </form>
+        <div id="main-is">
+            <h1>MI RED SOCIAL :D</h1>
+            <form id="iniciar-form">
+                <h2>INICIAR SESION</h2>
+                <input type="email" placeholder="Ingresa email" name="email" id="email"/>
+                <input type="password" placeholder="Ingresa contraseña" name="password" id="password"/>
+                <Button variant="contained" color="primary">INICIAR SESION</Button>
+                <a href="#">Registrarse</a>
+            </form>
+        </div>
     )
 }
 
